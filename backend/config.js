f7e683330943760bc14c61f2fdcc064bdc9afbe2
IamPiyush03 <piyushdhondge03@gmail.com>
2024-05-06 16:58:39 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+`mongodb+srv://piyushdhondge03:marsmission11@book-store-mern.z0c0wlb.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN`;
